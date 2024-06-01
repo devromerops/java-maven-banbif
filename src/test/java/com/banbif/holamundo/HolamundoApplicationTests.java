5701341bc06b9036f39bafe5501032eb0107e061
Johan Romero Aguirre <extjohanromero@belcorp.biz>
2024-05-31 20:52:05 -0500

    Devops: se sube primera versión

---
+++ src/test/java/com/banbif/holamundo/HolamundoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banbif.holamundo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolamundoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
