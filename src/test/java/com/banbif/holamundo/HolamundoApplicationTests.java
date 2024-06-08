0f53798caa853e8ed8f3d4c1f296c5e1c0f20a2a
Johan Romero Aguirre <johanrom12@gmail.com>
2024-06-08 13:37:55 -0500

    Añadido revision de reporte

--- src/test/java/com/banbif/holamundo/HolamundoApplicationTests.java
+++ src/test/java/com/banbif/holamundo/HolamundoApplicationTests.java
@@ -6,22 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class HolamundoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
-}
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class HolamundoApplicationTests {
-
     @Test
-    public void testTwo() {
-        String[] args = {}; // Provide any necessary arguments
-        HolamundoApplication.two(args);
-        // Add assertions or verification logic here if needed
+    void contextLoads() {
     }
 }
